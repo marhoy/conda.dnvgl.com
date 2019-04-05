61d0012d0123a3530c5893979066968d17df4fb1
Martin Hoy <martin.hoy@dnvgl.com>
2019-04-05 12:56:26 +0200

    Initial commit

--- restapi/endpoints.py
+++ restapi/endpoints.py
@@ -3,5 +3,7 @@ import subprocess
 
 def conda_index():
     output = subprocess.run("conda index /condarepo/stable", capture_output=True, shell=True)
-
-    return "StdErr:\n{}\n\nStdOut:\n{}".format(output.stderr, output.stdout)
+    return_string = "StdErr:\n{}\n\nStdOut:\n{}".format(
+        output.stderr.decode('UTF.8'),
+        output.stdout.decode('UTF-8'))
+    return return_string
